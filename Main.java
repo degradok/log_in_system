d40c315a04a60d65d187e5b123d510b2b05cc486
degradok <107347662+degradok@users.noreply.github.com>
2022-06-12 13:04:50 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import static org.example.log_in.logn;
+import static org.example.reg.regs;
+
+public class Main {
+    public static void main(String[] args) {
+        regs();
+    }
+}
